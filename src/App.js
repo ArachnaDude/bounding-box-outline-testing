066e3fd6bcd18d79a58b0acbbd7c4d57c3a88187
u1757390 <u1757390@unimail.hud.ac.uk>
2022-02-24 15:28:22 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+import geoJsonData from "./data/manchesterSmallData.json";
+import Clusters from "./Cluster";
 
 function App() {
+  const onEachNode = (node, layer) => {
+    // console.log(node.properties);
+    const nodeName = node.properties.name;
+
+    layer.bindPopup((nodeName || "Not found") + " ");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MapContainer
+        className='leaflet-container'
+        center={[53.483959, -2.244644]}
+        zoom={18}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        <GeoJSON data={geoJsonData.features} onEachFeature={onEachNode} />
+        <Clusters />
+      </MapContainer>
     </div>
   );
 }
